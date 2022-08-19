7e3fc70447ef4329582aad0129d6463c8fdacc4d
Lucas Barque da Silva <lucasbarquedasilva@hotmail.com>
2022-08-19 15:06:14 -0400

    main: backend finalizado

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,37 +1,19 @@
 import express from 'express';
-import { getRepository } from 'typeorm';
-import Orphanage from './models/Orphanage';
+import 'express-async-errors';
+import path from 'path';
+import cors from 'cors';
 
 import './database/connection';
+import errorHandler from './errors/handler';
+
+import routes from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-
-app.post('/orphanages', async (request, response) => {
-  const {
-    name,
-    latitude,
-    longitude,
-    about,
-    instructions,
-    opening_hours,
-    open_on_weekends,
-  } = request.body;
-
-  const orphanagesRepository = getRepository(Orphanage);
-
-  const orphanage = orphanagesRepository.create({
-    name,
-    latitude,
-    longitude,
-    about,
-    instructions,
-    opening_hours,
-    open_on_weekends,
-  });
-
-  await orphanagesRepository.save(orphanage);
-});
+app.use(routes);
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+app.use(errorHandler);
 
 app.listen(3333);
